15eaa4af275a5d446dfaf64749aa0bbe0cf23da8
jameskyokushin <james@kyokushin.com.ph>
2011-10-15 11:55:05 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shipment::Application.config.secret_token = '3509e0308ce6cdd0f112739ad4d8b581982fb0f2afb0fa32e5da3e32bc756c80eb621eaec672edcc6be0b0ba04aa0e3489675f435318052ced9216407d07a0a3'
